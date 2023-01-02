3423a5023690ed0e05c00146c594ebafee50730e
KamilZ99 <kamperv13@gmail.com>
2023-01-02 15:53:52 +0100

    Init

---
+++ BookshopWeb/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using BookshopWeb.Data;
+using BookshopWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookshopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
